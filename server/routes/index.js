023f823e618bdd7bed3830125ceb54cb3978cb78
KyleRattet <krattet@gmail.com>
2015-08-20 20:48:28 -0600

    updated submit form, and added success message so user knows the form was submitted

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/submit', function(req, res, next) {
+  //on submit route
+  // res.send('You submitted ' + req.body.email);
+
+
+  //redirect to success route on success
+  res.redirect('/success');
+
+});
+
+//success route, print success on browswer for user
+router.get('/success', function(req, res) {
+  res.send('success');
 });
 
 module.exports = router;
